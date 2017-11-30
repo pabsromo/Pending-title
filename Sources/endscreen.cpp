cc36a2a2602af43c746e0997ae7c41344e7bc74e
clarksnider <33442923+clarksnider@users.noreply.github.com>
2017-11-30 01:03:40 -0600

    Update endscreen.cpp

--- Sources/endscreen.cpp
+++ Sources/endscreen.cpp
@@ -1,9 +1,5 @@
 // endscreen.cpp
-// Clark Snider
-// 825004646
-// This is intended to display a screen after the tiles out of place reaches zero
 
-#include "fltk.h"
 #include "Graph.h"
 #include "GUI.h"
 #include "Point.h"
@@ -14,65 +10,68 @@
 
 using namespace Graph_lib;
 
-struct Endscreen_window: Graph_lib:: Window{
+struct Endscreen_window: Graph_lib:: Window{  // window opens if no remaining moves
 	Endscreen_window(Point xy, int w, int h, const string& title);
-	void wait_for_button()
-	{
+	void wait_for_button(){
 	while (!button_pushed) Fl::wait(); // wait for enter button to be pressed
 		button_pushed = false;         // before exiting the window...
 		hide();
 	};
-	
 private: // initialize buttons and in box
 	Button replay_button;
 	Button exit_button;
-		
+	Text first;
+	Text second;
+	Text third;
+	Text fourth;
+	Text fifth;
+	Text current;
+	gogogo = 0;
 	void replay();
 	void exit();
-	
-	
 };
 
-int main()
-try {
-		Endscreen_window win{ Point{ 0,0 },500,700,"Endscreen" };
-		win.wait_for_button();
-
-		return gui_main();
-
-	//  return 0;
-}
-catch (exception& e) {
-	cerr << "exception: " << e.what() << '\n';
-	return 1;
-}
-catch (...) {
-	cerr << "Some exception\n";
-	return 2;
-}
-
-
-
-Initials_window::Initials_window(Point xy, int w, int h, const string& title) // initialize the buttons' locations and callbacks
+Endscreen_window::Endscreen_window(Point xy, int w, int h, const string& title) // initialize the buttons' locations and callbacks
 	:Window{ xy,w,h,title },
-	replay_button{Point{ 200,525 },100,50,"Enter", [](Address,Address pw) {reference_to<Initials_window>(pw).enter(); } },
-	exit_button{Point{ 200,600 },100,50,"Exit", [](Address,Address pw) {reference_to<Initials_window>(pw).exit(); } },
+	replay_button{Point{ 200,525 },100,50,"Enter", [](Address,Address pw) {reference_to<Endscreen_window>(pw).enter(); } },
+	exit_button{Point{ 200,600 },100,50,"Exit", [](Address,Address pw) {reference_to<Endscreen_window>(pw).exit(); } },
+	first  {Point{ 100,50  }, score_one},
+	second {Point{ 100,125 }, score_two},
+	third  {Point{ 100,200 }, score_three},
+	fourth {Point{ 100,275 }, score_four},
+	fifth  {Point{ 100,350 }, score_five},
+	current  {Point{ 100,425 }, score_six}
+	
 	{
 	// attach each widget
 	attach(replay_button);
 	attach(exit_button);
-	}
+	attach(first);
+	attach(second);
+	attach(third);
+	attach(fourth);
+	attach(fifth);
+	attach(current);
+	}	
 	
 void Endscreen_window::replay() {
 	// This function needs to restart the program from the first window
+	button_pushed = true;
+	gogogo = 1;
 	hide();
 }
 
 void Endscreen_window::exit() {
 	// hide closes the program
+	button_pushed = true;
 	hide();
 }
-
-
-
-
+bool Endscreen_window_window::wait_for_button(){
+#if 1
+    // Simpler handler
+    while (!button_pushed) Fl::wait();
+    Fl::redraw();
+#else
+#endif
+    return button_pushed;
+}
